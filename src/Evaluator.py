6b0981df0ba7d839d11c99078de4aed67c6b53e8
Aum Garasia <aumgarasia@Aums-MacBook-Air.local>
2024-04-28 15:40:36 -0700

    Integrated and cleaned evaluator.py

--- src/Evaluator.py
+++ src/Evaluator.py
@@ -1,113 +1,92 @@
-# stmt_list(dec(int, id(x)), stmt_list(assign(id(y), t_add(2, 5)), display(id(y))))
-
-
 class Evaluator:
 
+    # Initializing the evaluator
     def __init__(self):
         self.env = {}
         self.functions = {}
         self.defaultValues = {'int': 0, 'str': '', 'bool': False}
         self.returnValue = 0
 
+    # Setting up constructors for the data types
     def setEnv(self, id, type, val):
-        self.env[id] = {'type': type, 'val': val}
+        self.env[id] = {'Type': type, 'Val': val}
 
-    def readTree(self, tree):
-        tree = tree.strip()
+    def readTree(self, T):
+        T = T.strip()
         leaves = []
         start = 0
-        # NODE
+        # Reading tree nodes
         i = start
-        while i < len(tree):
-            if tree[i] == '(':
+        while i < len(T):
+            if T[i] == '(':
                 node = tree[:i].strip()
                 start = i+1
                 break
             i += 1
-        #print('NODE: ', node)
-        # CHILDREN
+        
+        # Reading children nodes
         i = start
-        bracketsCount = 0
-        while i < len(tree):
-            if tree[i] == '(':
-                bracketsCount += 1
-            elif tree[i] == ')':
-                bracketsCount -= 1
-                if bracketsCount == -1:
-                    leaves.append(tree[start:i].strip())
-            elif tree[i] == ',' and bracketsCount == 0:
-                leaves.append(tree[start:i].strip())
+        paranthesisCount = 0
+        while i < len(T):
+            if T[i] == '(':
+                paranthesisCount += 1
+            elif T[i] == ')':
+                paranthesisCount -= 1
+                if paranthesisCount == -1:
+                    leaves.append(T[start:i].strip())
+            elif T[i] == ',' and paranthesisCount == 0:
+                leaves.append(T[start:i].strip())
                 start = i+1
             i += 1
         return node, leaves
 
+    # Evaluating the parse Tree
     def evaluate(self, tree):
         node, leaves = self.readTree(tree)
-        #print(node, " : ", leaves)
-        # print(self.env)
-        # print('*******************')
-
-        # NUMBERS
+        
+        # Evaluating numbers
         if node == 'num':
-            val = int(leaves[1])
-            if leaves[0] == 'neg':
+            val = int(leaves[1])    # Second node contains the value
+            if leaves[0] == 'neg':  # If negative number
                 val *= -1
             return val
 
-        # STRING
+        # 
         elif node == 'str':
             return leaves[0]
-
-        # BOOLEAN
         elif node == 'bool':
-            return leaves[0] == 'True'
-
-        # IDENTIFIERS
+            return leaves[0] == 'Sahi'
         elif node == 'id':
-            # TODO: init error
             return self.env[leaves[0]]['val']
 
-        # ARITHMETIC EXPRESSIONS
+        # Evaluating Arithmetic expressions
         elif node == 't_add':
-            # TODO: data type errors
             return self.evaluate(leaves[0]) + self.evaluate(leaves[1])
         elif node == 't_sub':
-            # TODO: data type errors
             return self.evaluate(leaves[0]) - self.evaluate(leaves[1])
         elif node == 't_mul':
-            # TODO: data type errors
             return self.evaluate(leaves[0]) * self.evaluate(leaves[1])
         elif node == 't_div':
-            # TODO: data type errors
             return int(self.evaluate(leaves[0]) / self.evaluate(leaves[1]))
         elif node == 't_mod':
-            # TODO: data type errors
             return self.evaluate(leaves[0]) % self.evaluate(leaves[1])
 
-        # BOOLEAN EXPRESSIONS
+        # Evaluating Booleans
         elif node == 'or':
-            # TODO: type errors
             return self.evaluate(leaves[0]) or self.evaluate(leaves[1])
         elif node == 'and':
-            # TODO: type errors
             return self.evaluate(leaves[0]) or self.evaluate(leaves[1])
         elif node == 'equals':
-            # TODO: type mismatch error
             return self.evaluate(leaves[0]) == self.evaluate(leaves[1])
         elif node == 'lt':
-            # TODO: type mismatch error
             return self.evaluate(leaves[0]) < self.evaluate(leaves[1])
         elif node == 'gt':
-            # TODO: type mismatch error
             return self.evaluate(leaves[0]) > self.evaluate(leaves[1])
         elif node == 'lteq':
-            # TODO: type mismatch error
             return self.evaluate(leaves[0]) <= self.evaluate(leaves[1])
         elif node == 'gteq':
-            # TODO: type mismatch error
             return self.evaluate(leaves[0]) >= self.evaluate(leaves[1])
         elif node == 'not':
-            # TODO: type mismatch error
             return not self.evaluate(leaves[0])
 
         # STATEMENTS
@@ -122,38 +101,37 @@ class Evaluator:
             # TODO: multiple init errors
             self.setEnv(leaves[1], leaves[0], self.evaluate(leaves[2]))
 
-        # assignment
+        # Evaluating syntactic sugar assignments
         elif node == 'assign':
-            # TODO: type mismatch errors, init errors
             self.env[leaves[0]]['val'] = self.evaluate(leaves[1])
         elif node == 'addAssign':
-            # TODO: type mismatch errors, init errors
             self.env[leaves[0]]['val'] += self.evaluate(leaves[1])
         elif node == 'subAssign':
-            # TODO: type mismatch errors, init errors
             self.env[leaves[0]]['val'] -= self.evaluate(leaves[1])
         elif node == 'mulAssign':
-            # TODO: type mismatch errors, init errors
             self.env[leaves[0]]['val'] *= self.evaluate(leaves[1])
         elif node == 'divAssign':
-            # TODO: type mismatch errors, init errors
             self.env[leaves[0]]['val'] = int(self.evaluate(
                 self.env[leaves[0]]['val'] / leaves[1]))
+        
+        # Displaying strings
         elif node == 'display':
             print(self.evaluate(leaves[0]))
-        # if-else
+        
+        # Evaluating Conditionals
         elif node == 'ifelse':
             if self.evaluate(leaves[0]):
                 self.evaluate(leaves[1])
             else:
                 self.evaluate(leaves[2])
+
         # for-loop
-        elif node == 'forT':
+        elif node == 'forT':    # Traditional for loop
             self.evaluate(leaves[0])
             while(self.evaluate(leaves[1])):
                 self.evaluate(leaves[2])
                 self.evaluate(leaves[3])
-        elif node == 'forR':
+        elif node == 'forR':    # Ranged for loop
             iterator = self.env[leaves[0]]
             iterator['val'] = self.evaluate(leaves[1])
             stopVal = self.evaluate(leaves[2])
@@ -161,16 +139,15 @@ class Evaluator:
                 self.evaluate(leaves[3])
                 iterator['val'] += 1
 
-        # FUNCTIONS
-        elif node == 'noneFunc':
+        # Evaluating functions
+        elif node == 'noneFunc':    # No functions are defined in program
             pass
-        elif node == 'funcList':
+        elif node == 'funcList':    # If there are functions instantiated
             self.evaluate(leaves[0])
             self.evaluate(leaves[1])
         elif node == 'func':
             self.defineFunc(leaves)
-        elif node == 'call':
-            #print('call', leaves)
+        elif node == 'call':        # When functions are called from the main body
             functionEvaluator = Evaluator()
             func = self.functions[leaves[0]]
             args = []
@@ -185,7 +162,7 @@ class Evaluator:
         elif node == 'return':
             self.returnValue = self.evaluate(leaves[0])
 
-        # PROGRAM
+        # Evaluate the program
         elif node == 'prog':
             self.evaluate(leaves[0])
             self.evaluate(leaves[1])
@@ -213,33 +190,12 @@ class Evaluator:
             Accumulator.append(self.evaluate(tree))
 
     def defineFunc(self, leaves):
-        funcName = leaves[0]  # bloo
+        funcName = leaves[0]
         pmtList = []
-        # [['int', 'a'], ['int', 'b']]
-        self.parseParameters(leaves[1], pmtList)
+        self.parseParameters(leaves[1], pmtList)    # Get parameters from the parse tree
         funcTree = leaves[2]
         self.functions[funcName] = {'parameters': pmtList, 'tree': funcTree}
 
 
 if __name__ == '__main__':
     eval = Evaluator()
-    eval.env['x'] = {'type': 'int', 'val': 5}
-    s = 'stmt_list(dec(int, id(x)), stmt_list(assign(id(y), t_add(2, 5)), display(id(y))))'
-    s1 = 'display(id(y))'
-    s2 = 'forT( assign(id(x), 0), lt(id(x), 10), assign(id(x), t_add(id(x), 1)), stmt_list( display(id(x)), display(id(y)) ) )'
-    s3 = 'id(x)'
-    s4 = 'num(neg,6)'
-
-    # print(eval.evaluate(s4))
-
-    # acc = []
-    # eval.parseParameters('pmtList(pmt(int, a), pmt(int, b))', acc)
-    # print(acc)
-
-    f1 = ['bloo', 'pmtList(pmt(int, a), pmt(int, b))',
-          'stmt_list(dec(int, c), stmt_list(assign(c, t_add(id(a), id(b))), return(id(c))))']
-
-    c1 = ['bloo', 'argList(id(x), num(pos, 6))']
-    acc = []
-    eval.parseArguments('argList(id(x), num(pos, 6))', acc)
-    print(acc)
