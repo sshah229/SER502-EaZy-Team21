1fcf50417312edffcd3eb27adf8cab115e34531e
Aum Garasia <aumgarasia@Aums-MacBook-Air.local>
2024-04-28 15:19:46 -0700

    Finalized Main Runtime

--- src/EaZy.py
+++ src/EaZy.py
@@ -3,24 +3,30 @@ from pyswip import Prolog
 
 sys.path.insert(0, os.getcwd() + '/src/compiler')
 
-from tokenizer import Tokenizer         # These files are assumed to be made at this point
-# from evaluator import Evaluator
+from tokenizer import Tokenizer
+from Evaluator import Evaluator
 
 if __name__=='__main__':
+     # Open a program to Read
      file = open("sample.ez", "r")
      program =  file.read()
      
+     # Tokenize the program according to grammar
      Tk = Tokenizer()
      tokens = Tk.tokenizeProgram(program)
-     print(tokens)
 
+     # Consult the parser to then generate a prolog query
      prolog = Prolog()
      prolog.consult('parser.pl') 
      query = "program(T, " + str(tokens) + ", [])."
-     print(query)
 
-     parseTree = ''
+     # Generating a parse tree for the query
      for soln in prolog.query(query):
           parseTree = soln['T']
           break
-     print(parseTree)
+
+     # Evaluate and print out final environment
+     eval = Evaluator()
+     eval.evaluate(parseTree)
+     print('Execution SUCCESS :)')
+     print('Environment: ', eval.env)
