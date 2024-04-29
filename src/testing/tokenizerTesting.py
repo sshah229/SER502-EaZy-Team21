49e40a8e41a7ac63301b4c311da18ade744bb4bc
shubhamshah15 <you@example.com>
2024-04-28 19:17:26 -0700

    Added cases to test more functionality.

--- src/testing/tokenizerTesting.py
+++ src/testing/tokenizerTesting.py
@@ -3,7 +3,7 @@ from unittest import result
 import os, sys
 
 sys.path.insert(0, os.getcwd() + '/src/')
-from tokenizer import tokenise
+from tokenizer import Tokenizer
 
 
 class Testing(unittest.TestCase):
@@ -18,22 +18,61 @@ class Testing(unittest.TestCase):
 
     testCase_4 = "sup\n int N;\n str SASS;"
     expected_4 = ['sup', 'int', 'N', ';','str', 'SASS', ';']
+    
+    testCase_5 = "bool X = Sahi or Galat;"
+    expected_5 = ['bool', 'X', '=', 'Sahi', 'or', 'Galat', ';']
+    
+    testCase_6 = "x == 5 ? display Sahi : display Galat;"
+    expected_6 = ['x', '==', '5', '?', 'display', 'Sahi', ':', 'display', 'Galat', ';']
+    
+    testCase_7 = "if (x == 5) { display Sahi; } else { display Galat; }"
+    expected_7 = ['if', '(', 'x', '==', '5', ')', '{', 'display', 'Sahi', ';', '}', 'else', '{', 'display', 'Galat', ';', '}']
+    
+    testCase_8 = "for-loop (int i = 0; i < 5; i += 1) { display i; }"
+    expected_8 = ['for-loop', '(', 'int', 'i', '=', '0', ';', 'i', '<', '5', ';', 'i', '+=', '1', ')', '{', 'display', 'i', ';', '}']
+    
+    testCase_9 = "func int add(int a, int b) { return a + b; }"
+    expected_9 = ['func', 'int', 'add', '(', 'int', 'a', ',', 'int', 'b', ')', '{', 'return', 'a', '+', 'b', ';', '}']
 
     def test1(self):
-        result = tokenise(Testing.testCase_1)
+        result = Tokenizer(Testing.testCase_1)
         self.assertEqual(result, Testing.expected_1)
     
     def test2(self):
-        result = tokenise(Testing.testCase_2)
+        result = Tokenizer(Testing.testCase_2)
         self.assertEqual(result, Testing.expected_2)
 
     def test3(self):
-        result = tokenise(Testing.testCase_3)
+        result = Tokenizer(Testing.testCase_3)
         self.assertEqual(result, Testing.expected_3)
     
     def test4(self):
-        result = tokenise(Testing.testCase_4)
-        self. assertEqual(result, Testing.expected_4)
+        result = Tokenizer(Testing.testCase_4)
+        self.assertEqual(result, Testing.expected_4)
+    
+    def test4(self):
+        result = Tokenizer(Testing.testCase_4)
+        self.assertEqual(result, Testing.expected_4)
+    
+    def test5(self):
+        result = Tokenizer(Testing.testCase_5)
+        self.assertEqual(result, Testing.expected_5)
+    
+    def test6(self):
+        result = Tokenizer(Testing.testCase_6)
+        self.assertEqual(result, Testing.expected_6)
+    
+    def test7(self):
+        result = Tokenizer(Testing.testCase_7)
+        self.assertEqual(result, Testing.expected_7)
+    
+    def test8(self):
+        result = Tokenizer(Testing.testCase_8)
+        self.assertEqual(result, Testing.expected_8)
+    
+    def test9(self):
+        result = Tokenizer(Testing.testCase_9)
+        self.assertEqual(result, Testing.expected_9)
 
 if __name__ == '__main__':
     unittest.main()
